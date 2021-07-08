3dbf309806fad22469464ba8d7e07d2fda14f53e
sazonovme <mr.loraks@bk.ru>
2021-07-08 14:44:25 +0600

    create: open/close cart

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,24 @@ headerCityButton.addEventListener('click', () => {
     const city = prompt('Укажите ваш город');
     headerCityButton.textContent = city; 
     localStorage.setItem('lomoda-location', city);
-});
+}); // Для кнопки твоего города 
+
+const subheaderCart = document.querySelector('.subheader__cart'); //Переменная корзины 
+const cartOverlay = document.querySelector('.cart-overlay');//Переменная модального окна 
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open'); // Добавление класса, чтобы открыть модальное окно
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open'); // Функция для закрытия модального окна
+};
+
+subheaderCart.addEventListener('click', cartModalOpen); // Открытие модального окна 
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+    if(target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')){
+        cartModalClose();
+    }
+}); // Закрытие модального окна 
